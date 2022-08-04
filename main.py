498fd54a12bb288d81950ae90e79fb671fcc93c2
bceda <109223555+bceda@users.noreply.github.com>
2022-08-04 10:19:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#
+# Import dependencies
+#
+
+#
+# User Interface (UI)
+# Requirements: The User Interface should ask the user to select a date in a 6-month range either side of the current date.
+# Additionally it should default to today's date.
+
+# User Interface stores the user's option in a variable.
+
+# User's option is translated into the file's name.
+
+#
+# FTP
+#
+
+# Establish FTP connection
+
+# Check file exists on the FTP Server.
+# File does not exist, error handling.
+# File does exist, proceed.
+
+# Download the file.
+
+#
+# Data Validation
+#
+
+# Validate the file.
+# Name validation has already occured.
+
+# Lastly, open the file directory.
