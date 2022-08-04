5d9b4580934c41a7dfee492de7d8b1557b8b76cd
bceda <109223555+bceda@users.noreply.github.com>
2022-08-04 10:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 #
 # Import dependencies
 #
+from tkinter import *
 
 #
 # User Interface (UI)
 # Requirements: The User Interface should ask the user to select a date in a 6-month range either side of the current date.
 # Additionally it should default to today's date.
+
+window = Tk()
+
+window.title("Date Selection")
+
+window.geometry('1000x600')
+window.configure(bg='aqua')
+
+lbl = Label(window, text="Day")
+lbl.grid(column=0, row=0)
+lbl = Label(window, text="Hello", font=("Arial Bold", 50))
+var =IntVar()
+var.set(3)
+spin = Spinbox(window, from_=1, to=31, width=5, textvariable=var)
+spin.grid(column=10,row=0)
+
+lbl = Label(window, text="Month")
+lbl.grid(column=0, row=20)
+lbl = Label(window, text="Hello", font=("Arial Bold", 50))
+var =IntVar()
+var.set(8)
+spin = Spinbox(window, from_=1, to=12, width=5, textvariable=var)
+spin.grid(column=10,row=20)
+
+lbl = Label(window, text="Year")
+lbl.grid(column=0, row=40)
+lbl = Label(window, text="Hello", font=("Arial Bold", 50))
+var =IntVar()
+var.set(2022)
+spin = Spinbox(window, from_=2020, to=2023, width=5, textvariable=var)
+spin.grid(column=10,row=40)
+
+window.mainloop()
 
 # User Interface stores the user's option in a variable.
 
