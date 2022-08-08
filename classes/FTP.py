59d5a867969b2c2bd17d28ed7efc09dd0add36e5
bceda <109223555+bceda@users.noreply.github.com>
2022-08-08 19:30:52 +0100

    Add files via upload

--- classes/FTP.py
+++ classes/FTP.py
@@ -2,23 +2,26 @@ import ftplib;
 
 class FTP:
   
-    def __init__(self, host, port, username, password ):
+    def __init__(self, host, username, password ):
 
         self.ftp = ftplib.FTP( host )
         self.ftp.login( username, password )
+        self.download_path = '/'
 
-    def close():
+    def close( self ):
         self.ftp.quit()
 
-    def download( file_name ):
+    def download( self, file_name, destination_path ):
         # Check file exists on the FTP Server.
         try:
             resp = self.ftp.sendcmd( ( "MLST " + file_name + ".csv" ) )
         except:
-            # File does not exist, error handling.
-            print("Error. The file specified does not exist on the server.")
+            # File does not exist, throw a FileNotFoundError.
+            raise FileNotFoundError("Error. The file specified does not exist on the server.")
 
         # File does exist, proceed.
         if 'type=file;' in resp:
-            with open( download_path + file_name + ".csv", 'wb') as f:
+            with open( destination_path + file_name + ".csv", 'wb') as f:
                 self.ftp.retrbinary('RETR ' + file_name + ".csv", f.write)
+
+        return destination_path + file_name + ".csv"
